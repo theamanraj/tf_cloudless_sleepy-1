15abedb1c7e073e3c40534a5406f48dc70fa0e94
Jagannadha Srikar Uppada <srikarseven@gmail.com>
2019-11-07 15:43:15 +0530

    test pull latest

--- vars.tf
+++ vars.tf
@@ -9,3 +9,8 @@ variable "sleepy_time" {
   description = "How long our local-exec will take a nap."
   default     = 0
 }
+
+variable "sleepy_time2" {
+  description = "How long our local-exec will take a nap."
+  default     = 0
+}
