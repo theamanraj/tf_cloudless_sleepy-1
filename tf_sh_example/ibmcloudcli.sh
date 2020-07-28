2093266efdae26e2be4c8b5d4864fb54551dd9f6
singhdeepu <deepusingh1130@gmail.com>
2020-07-28 11:27:16 +0530

    Update ibmcloudcli.sh

--- tf_sh_example/ibmcloudcli.sh
+++ tf_sh_example/ibmcloudcli.sh
@@ -2,6 +2,7 @@
 whoami
 echo "checking ob plugin"
 ibmcloud ob
+mv home/nobody/.terraform.d/plugins/terraform-provider-ibm_v1.9.0 home/nobody/.terraform.d/plugins/terraform-provider-ibm_v1.9.0_bkp
 
 ps aux
 sleep 10
