15aac331ef65f790ae2f8e09bb3c725ac53a3eda
singhdeepu <deepusingh1130@gmail.com>
2020-08-06 00:59:01 +0530

    Update usr_dir_permission.sh

--- tf_sh_example/usr_dir_permission.sh
+++ tf_sh_example/usr_dir_permission.sh
@@ -5,7 +5,7 @@ echo "Testing Temp Directory permissions"
 
 mkdir -p /usr/local/bin/testDir
 if [ $? -ne 0 ] ; then
-  echo "FAILED: appuser has no write permission"
+  echo "FAILED: appuser has no write permission in usr dir"
 fi
 
 cd /usr/local/bin
