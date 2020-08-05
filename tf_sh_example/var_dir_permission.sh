fa987ad06d6e1a223ec8ff967957c4f19b78c81e
singhdeepu <deepusingh1130@gmail.com>
2020-08-05 22:20:37 +0530

    Update var_dir_permission.sh

--- tf_sh_example/var_dir_permission.sh
+++ tf_sh_example/var_dir_permission.sh
@@ -7,25 +7,23 @@ mkdir -p /var/testDir
 if [ $? -ne 0 ] ; then
   echo "FAILED: appuser has no write permission"
 fi
-echo "PASSED: appuser has write permission to create a directory/file"
 
 mv /var/testDir /var/testDirRename
 if [ $? -ne 0 ] ; then
   echo "FAILED: appuser can't rename a directory/file"
 fi
-echo "PASSED: appuser is able to rename a directory/file"
+
 
 touch /var/testDirRename/create.txt
 if [ $? -ne 0 ] ; then
   echo "FAILED: appuser is not able to create a file"
 fi
-echo "PASSED: appuser is able to create a file"
 
 cat /var/testDirRename/create.txt
 if [ $? -ne 0 ] ; then
   echo "FAILED: appuser is not able to create a file"
 fi
-echo "PASSED: appuser is able to read a file"
+
 
 touch /var/testDirRename/a.sh
 chmod +x /var/testDirRename/a.sh
@@ -34,4 +32,3 @@ cd /var/testDirRename
 if [ $? -ne 0 ] ; then
   echo "FAILED: appuser is not able to execute a shell script"
 fi
- echo "PASSED: appuser is able to execute a shell script"
