e75aeef9cd84e127254851988e2b13af54fe505b
singhdeepu <deepusingh1130@gmail.com>
2020-08-06 00:56:51 +0530

    Update usr_dir_permission.sh

--- tf_sh_example/usr_dir_permission.sh
+++ tf_sh_example/usr_dir_permission.sh
@@ -5,7 +5,7 @@ echo "Testing Temp Directory permissions"
 
 mkdir -p /usr/local/bin/testDir
 if [ $? -ne 0 ] ; then
-  echo "FAILED: appuser has no write permission"
+  echo "FAILED: appuser has no write permission to usr dir"
 fi
 
 cd /usr/local/bin
@@ -13,4 +13,4 @@ echo '{"foo": 0}' | jq
 if [ $? -ne 0 ] ; then
   echo "FAILED: appuser is not able to execute pre installed app"
 fi
-echo "PASSED: appuser is able to execute a shell script"
+echo "PASSED: appuser is able to execute pre-installed apps"
