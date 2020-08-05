39600d5fdce0948c30f809e3405fb97e389dc3ba
singhdeepu <deepusingh1130@gmail.com>
2020-08-06 00:34:02 +0530

    Update and rename var_dir_permission.sh to usr_dir_permission.sh

---
+++ tf_sh_example/usr_dir_permission.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+  
+
+echo "Testing Temp Directory permissions"
+
+mkdir -p /usr/local/bin/testDir
+if [ $? -ne 0 ] ; then
+  echo "FAILED: appuser has no write permission"
+fi
+
+cd /usr/local/bin
+echo '{"foo": 0}' | jq
+if [ $? -ne 0 ] ; then
+  echo "FAILED: appuser is not able to execute pre installed app"
+fi
+echo "PASSED: appuser is able to execute a shell script"
