de6816f5c8cc6633e349eee18e63c67ac50dca0b
Deepu Singh <deepusi1@in.ibm.com>
2020-08-12 20:10:48 +0530

    cloudpak var dir permission

---
+++ tf_sh_example/var_dir_permission.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+  
+
+echo "Testing Var Directory permissions"
+
+mkdir -p /var/testDir
+if [ $? -ne 0 ] ; then
+  echo "FAILED: appuser has no write permission in var dir"
+fi
+
+mv /var/testDir /var/testDirRename
+if [ $? -ne 0 ] ; then
+  echo "FAILED: appuser can't rename a directory/file"
+fi
+
+
+touch /var/testDirRename/create.txt
+if [ $? -ne 0 ] ; then
+  echo "FAILED: appuser is not able to create a file"
+fi
+
+
+cat /var/testDirRename/create.txt
+if [ $? -ne 0 ] ; then
+  echo "FAILED: appuser is not able to create a file"
+fi
+
+
+touch /var/testDirRename/a.sh
+chmod +x /var/testDirRename/a.sh
+cd /var/testDirRename
+./a.sh
+if [ $? -ne 0 ] ; then
+  echo "FAILED: appuser is not able to execute a shell script"
+fi
