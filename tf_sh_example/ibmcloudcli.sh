931e0ebbaeb2cb48c22f0dbbf60cbcffc5a946ba
singhdeepu <deepusingh1130@gmail.com>
2020-09-02 11:50:12 +0530

    All cli added

--- tf_sh_example/ibmcloudcli.sh
+++ tf_sh_example/ibmcloudcli.sh
@@ -4,10 +4,54 @@ echo "checking dev plugin"
 ibmcloud dev
 echo "checking ob plugin"
 ibmcloud ob
-ibmcloud cs
 echo "checking cs plugin"
-ibmcloud cr
+ibmcloud cs
 echo "checking cr plugin"
+ibmcloud cr
+echo "checking hpvs plugin"
+ibmcloud hpvs
+echo "checking analytics-engine plugin"
+ibmcloud analytics-engine
+echo "checking vpn plugin"
+ibmcloud vpn
+echo "checking activity-tracker plugin"
+ibmcloud activity-tracker
+echo "checking machine-learning plugin"
+ibmcloud machine-learning
+echo "checking cloud-internet-services plugin"
+ibmcloud cloud-internet-services
+echo "checking dbaas-cli plugin"
+ibmcloud dbaas-cli
+echo "checking cloud-databases plugin"
+ibmcloud cloud-databases
+echo "checking key-protect plugin"
+ibmcloud key-protect
+echo "checking doi plugin"
+ibmcloud doi
+echo "checking tke plugin"
+ibmcloud tke
+echo "checking auto-scaling plugin"
+ibmcloud auto-scaling
+echo "checking event-streams plugin"
+ibmcloud event-streams
+echo "checking power-iaas plugin"
+ibmcloud power-iaas
+echo "checking vpc-infrastructure plugin"
+ibmcloud vpc-infrastructure
+echo "checking code-engine plugin"
+ibmcloud code-engine
+echo "checking cloud-dns-services plugin"
+ibmcloud cloud-dns-services
+echo "checking dl-cli plugin"
+ibmcloud dl-cli
+echo "checking watson plugin"
+ibmcloud watson
+echo "checking catalogs-management plugin"
+ibmcloud catalogs-management
+echo "checking tg-cli plugin"
+ibmcloud tg-cli
+echo "checking coligo plugin"
+ibmcloud coligo
 kubectl version
 echo "kubectl version"
 terraform version
