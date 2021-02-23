60a6dd3dd8afae098cb53a5648833d68b2ed8b5a
singhdeepu <deepusingh1130@gmail.com>
2021-02-23 19:24:01 +0530

    Update ibmcloudcli.sh

--- tf_sh_example/ibmcloudcli.sh
+++ tf_sh_example/ibmcloudcli.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-api_key=$1
 whoami
 echo "checking ob plugin"
 echo $api_key
-ic login --apikey $api_key
+ibmcloud login --apikey $api_key
 ibmcloud sat host attach --location us-east
 
 sleep 10
