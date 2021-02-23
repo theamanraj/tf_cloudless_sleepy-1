c6a6f15991f267aacc5b77d866267b915422c394
singhdeepu <deepusingh1130@gmail.com>
2021-02-23 19:08:27 +0530

    Update main.tf

--- tf_sh_example/main.tf
+++ tf_sh_example/main.tf
@@ -1,12 +1,12 @@
 # Runs a shell script
-
+variable "apikey" {
+	}
 resource "null_resource" "sleep" {
   triggers = {
 	uuid = "${uuid()}"
   }
 
   provisioner "local-exec" {
-    apikey = var.api_key
     command = "./ibmcloudcli.sh apikey"
   }
  }
